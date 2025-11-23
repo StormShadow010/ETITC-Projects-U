3115784d2c78a430121c1d3583aaaffd7255603d
Santiago <asjimenezg1923@gmail.com>
2025-11-22 23:44:46 -0500

    KanBan Completo

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,17 +3,16 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9oxW1B9HJrc5HWnoJocTXJ8DLH9aZOIE",
-  authDomain: "extensionkanban.firebaseapp.com",
-  projectId: "extensionkanban",
-  storageBucket: "extensionkanban.firebasestorage.app",
-  messagingSenderId: "737270979991",
-  appId: "1:737270979991:web:300d51c91d097b2760b8d2",
-  measurementId: "G-H10TCK37N5",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-
 export const googleProvider = new GoogleAuthProvider();
